3845e1344ee8ad21c6d791b0993379f022567edf
Diego Aranda <104237553+DiegoAlb09@users.noreply.github.com>
2025-08-28 07:47:57 -0600

    Parcial01 - Install EF Core for SQLite

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,5 +7,6 @@ public class AppUser
   public string Id { get; set; } = Guid.NewGuid().ToString();
 
   public required string DisplayName { get; set; }
+
   public required string Email { get; set; }
 }
