e4409be20fe02ce73df9d6f838e3a92196c4fb0c
DiegoAlb09 <diegoalb04hat@gmail.com>
2025-10-06 08:15:33 -0600

    Parcial02 - Add Exception Middleware

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ErrorController : BaseApiController
+{
+
+  [HttpGet("bad-request")]
+  public IActionResult GetBadRequest()
+  {
+    var inputParam = -1;
+    if (inputParam <= 0) throw new ArgumentOutOfRangeException(nameof(inputParam));
+
+    return BadRequest("Bad request");
+  }
+
+  [HttpGet("auth")]
+  public IActionResult GetAuth()
+  {
+    return Unauthorized();
+  }
+
+  [HttpGet("not-found")]
+  public IActionResult GetNotFound()
+  {
+    return NotFound();
+  }
+
+  [HttpGet("server-error")]
+  public IActionResult GetServerError()
+  {
+    throw new Exception("Server error");
+  }
+}
